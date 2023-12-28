95e15a8c1dfa753b18c056a3b72f8c1da592b18c
Andrus Adamchik <andrus@objectstyle.com>
2023-12-28 12:10:55 -0500

    JupyterTest is failing
    this was previously ignored as the test was not really run

--- src/test/java/com/nhl/dflib/docs/JupyterTest.java
+++ src/test/java/com/nhl/dflib/docs/JupyterTest.java
@@ -1,12 +1,22 @@
 package com.nhl.dflib.docs;
 
 import com.nhl.dflib.jupyter.DFLibJupyter;
+import io.github.spencerpark.jupyter.kernel.BaseKernel;
+import io.github.spencerpark.jupyter.kernel.display.Renderer;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class JupyterTest extends BaseTest {
 
     @Test
     public void setDisplayParams() {
+
+        BaseKernel kernel = mock(BaseKernel.class);
+        when(kernel.getRenderer()).thenReturn(new Renderer());
+
+        DFLibJupyter.init(kernel);
 
         // tag::setDisplayParams[]
         DFLibJupyter.setMaxDisplayRows(10);
