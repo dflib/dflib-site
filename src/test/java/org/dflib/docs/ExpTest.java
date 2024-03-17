8a9763f0cb17121437ac7cc749543e489e010754
Andrus Adamchik <andrus@objectstyle.com>
2024-03-17 11:51:53 -0400

    documenting constant expressions

--- src/test/java/org/dflib/docs/ExpTest.java
+++ src/test/java/org/dflib/docs/ExpTest.java
@@ -31,6 +31,39 @@ public class ExpTest extends BaseTest {
     }
 
     @Test
+    public void constantExp() {
+
+        DataFrame df = DataFrame.foldByRow("first", "last", "salary").of(
+                "Jerry", "Cosin", new BigDecimal("120000"),
+                "Juliana", "Walewski", new BigDecimal("80000"),
+                "Joan", "O'Hara", new BigDecimal("95000"));
+
+// tag::constantExp[]
+        Series<String> hi = $val("hi!").eval(df);
+// end::constantExp[]
+
+        print("hi", hi);
+    }
+
+    @Test
+    public void constantConcatExp() {
+
+        DataFrame df = DataFrame.foldByRow("first", "last", "salary").of(
+                "Jerry", "Cosin", new BigDecimal("120000"),
+                "Juliana", "Walewski", new BigDecimal("80000"),
+                "Joan", "O'Hara", new BigDecimal("95000"));
+
+// tag::constantConcatExp[]
+        Series<String> fn = concat(
+                $str("first"),
+                $val(" "), // <1>
+                $str("last")).eval(df);
+// end::constantConcatExp[]
+
+        print("constantConcatExp", fn);
+    }
+
+    @Test
     public void expChain() {
 
 // tag::expChain[]
