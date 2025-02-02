98cc41163ab64e386325e2e45c0493986810270b
Andrus Adamchik <andrus@objectstyle.com>
2025-02-02 11:14:24 -0500

    Window sort docs

--- dflib2-docs/src/test/java/org/dflib/docs/WindowTest.java
+++ dflib2-docs/src/test/java/org/dflib/docs/WindowTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.dflib.Exp.$int;
+import static org.dflib.Exp.*;
 
 
 public class WindowTest extends BaseTest {
 
     static final DataFrame df = DataFrame.foldByRow("name", "amount", "date").of(
-            "Jerry Cosin", 8000, LocalDate.of(2024, 1, 15),
+            "Jerry Cosin", 8800, LocalDate.of(2024, 1, 15),
             "Juliana Walewski", 8500, LocalDate.of(2024, 1, 15),
             "Joan O'Hara", 9300, LocalDate.of(2024, 1, 15),
-            "Jerry Cosin", 8800, LocalDate.of(2024, 2, 15),
-            "Juliana Walewski", 8500, LocalDate.of(2024, 2, 15),
-            "Joan O'Hara", 9500, LocalDate.of(2024, 2, 15));
+            "Jerry Cosin", 9500, LocalDate.of(2024, 12, 15),
+            "Juliana Walewski", 8500, LocalDate.of(2025, 2, 15),
+            "Joan O'Hara", 8000, LocalDate.of(2023, 1, 1));
 
     static {
         System.out.println(new TabularPrinter(8, 100).toString(df));
@@ -41,6 +41,19 @@ public class WindowTest extends BaseTest {
 // end::windowPartition[]
 
         print("windowPartition", df1);
+    }
+
+    @Test
+    public void windowPartitionSorting() {
+// tag::windowPartitionSorting[]
+        DataFrame df1 = df
+                .over()
+                .partitioned("name")
+                .sorted($col("date").asc()) // <1>
+                .cols("change_order").merge(rowNum()); // <2>
+// end::windowPartitionSorting[]
+
+        print("windowPartition_RowNum", df1);
     }
 
 
