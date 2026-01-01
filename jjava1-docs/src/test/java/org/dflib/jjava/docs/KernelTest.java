4cf13be7cdf34bae8ff2b713bfbd3ec6a8b03efc
Andrus Adamchik <andrus@objectstyle.com>
2025-12-31 20:51:47 -0500

    reorg of notebook functions docs

--- jjava1-docs/src/test/java/org/dflib/jjava/docs/KernelTest.java
+++ jjava1-docs/src/test/java/org/dflib/jjava/docs/KernelTest.java
@@ -4,6 +4,7 @@ import org.dflib.jjava.kernel.JavaKernel;
 import org.junit.jupiter.api.Test;
 
 import static org.dflib.jjava.jupyter.kernel.BaseNotebookStatics.display;
+import static org.dflib.jjava.jupyter.kernel.BaseNotebookStatics.updateDisplay;
 import static org.dflib.jjava.kernel.JavaNotebookStatics.kernel;
 
 public class KernelTest {
@@ -36,6 +37,45 @@ public class KernelTest {
             // tag::doDisplay[]
             display(o);
             // end::doDisplay[]
+
+        } finally {
+            kernel1.onShutdown(false);
+        }
+    }
+
+    @Test
+    public void doDisplayMime() {
+        JavaKernel kernel1 = JavaKernel.builder().build();
+
+        try {
+            kernel1.onStartup();
+
+            // tag::doDisplayMime[]
+            display("<b>bold</b>", "text/html");
+            display("_italic_", "text/markdown");
+            // end::doDisplayMime[]
+
+        } finally {
+            kernel1.onShutdown(false);
+        }
+    }
+
+    @Test
+    public void doUpdateDisplay() throws InterruptedException {
+        JavaKernel kernel1 = JavaKernel.builder().build();
+
+        try {
+            kernel1.onStartup();
+
+            // tag::doUpdateDisplay[]
+            String id = display("Countdown: 3");
+            for (int i = 3; i >= 0; i--) {
+                updateDisplay(id, "Countdown: " + i);
+                Thread.sleep(1000L);
+            }
+
+            display("Liftoff!");
+            // end::doUpdateDisplay[]
 
         } finally {
             kernel1.onShutdown(false);
