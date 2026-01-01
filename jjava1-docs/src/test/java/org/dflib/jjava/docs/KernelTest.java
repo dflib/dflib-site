77e6d7680bdcf73aeeadbfcff94ef0e591c4afe3
Andrus Adamchik <andrus@objectstyle.com>
2025-12-31 20:36:46 -0500

    minor edits

--- jjava1-docs/src/test/java/org/dflib/jjava/docs/KernelTest.java
+++ jjava1-docs/src/test/java/org/dflib/jjava/docs/KernelTest.java
@@ -3,6 +3,7 @@ package org.dflib.jjava.docs;
 import org.dflib.jjava.kernel.JavaKernel;
 import org.junit.jupiter.api.Test;
 
+import static org.dflib.jjava.jupyter.kernel.BaseNotebookStatics.display;
 import static org.dflib.jjava.kernel.JavaNotebookStatics.kernel;
 
 public class KernelTest {
@@ -24,15 +25,17 @@ public class KernelTest {
     }
 
     @Test
-    public void doEval() {
+    public void doDisplay() {
         JavaKernel kernel1 = JavaKernel.builder().build();
 
         try {
             kernel1.onStartup();
 
-            // tag::doEval[]
-            kernel().eval("1 + 2");
-            // end::doEval[]
+            Object o = new Object();
+
+            // tag::doDisplay[]
+            display(o);
+            // end::doDisplay[]
 
         } finally {
             kernel1.onShutdown(false);
